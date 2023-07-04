693c5b7b2d4ef56f7bf5b1bf6da61dfecea7c258
imLulu8 <lucac8mmendatore@gmail.com>
2023-07-04 11:36:03 +0200

    feature():addInterceptor_MethodSignIn_SaveTokenOnHeaders

--- src/app/components/signin/signin.component.ts
+++ src/app/components/signin/signin.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ApiAuthService } from 'src/app/services/api-auth.service';
 
@@ -9,52 +9,45 @@ import { ApiAuthService } from 'src/app/services/api-auth.service';
   styleUrls: ['./signin.component.scss']
 })
 export class SignInComponent implements OnInit {
-  loginform!: FormGroup
+  loginForm!: FormGroup;
   isLogged = false;
   loginError: string | null = null;
 
-  // @ViewChild('loginform') loginform!: NgForm   
-  constructor(private router: Router, private apiAuthServices: ApiAuthService) { }
+  constructor(private router: Router, private apiAuthService: ApiAuthService) { }
 
-
-  //NG ON INIT PER REACTIVEFORM
   ngOnInit(): void {
-    this.loginform = new FormGroup({
+    this.loginForm = new FormGroup({
       email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, [Validators.required]),
-    })
+    });
   }
 
-
-
-
   onSubmit() {
-    if (this.loginform.valid) {
-      const loginData = this.loginform.value;
-      this.apiAuthServices.signin(loginData).subscribe(
+    if (this.loginForm.valid) {
+      const loginData = this.loginForm.value;
+      this.apiAuthService.signin(loginData).subscribe(
         (response: any) => {
-          // Registrazione avvenuta con successo, puoi gestire l'azione successiva qui
-          this.isLogged = true
-          this.loginform.reset();
-          console.log('DATA ', loginData)
+          console.log('Response:', response);
+          const token = response.token;
 
+          // Salva il token nel localStorage
+          localStorage.setItem('token', token);
 
-          // setTimeout(() => {
-          //   this.router.navigate(['/characters']);
-          // }, 3000);
+          this.isLogged = true;
+          this.loginForm.reset();
 
-
+          setTimeout(() => {
+            this.router.navigate(['/characters']);
+          }, 3000);
         },
         (error: any) => {
-          // Gestisci l'errore di registrazione qui
-          if (error.error.message === 'Invalid Credential') {
-            this.loginError = 'Unhautorized';
+          // Gestisci l'errore di login qui
+          if (error.error.message === 'Invalid credentials') {
+            this.loginError = 'Unauthorized';
             this.isLogged = false;
           }
         }
       );
     }
   }
-
-
 }
--- src/app/services/auth-interceptor.service.ts
+++ src/app/services/auth-interceptor.service.ts
@@ -8,14 +8,16 @@ export class AuthInterceptorService implements HttpInterceptor {
   constructor() { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    console.log("🚀~ intercept ~ request:", request);
-    if (request.method === 'POST' 
-      || request.method === 'PATCH' 
-      || request.method === 'DELETE'
-      || !request.url.includes('login')) {
+    // console.log("🚀~ intercept ~ request:", request);
+    
+    // Controlla se l'utente è loggato (puoi personalizzare questa logica in base alla tua implementazione di autenticazione)
+    const isLogged = localStorage.getItem('token') !== null;
+
+    if (isLogged) {
       // Ottieni il token dal localStorage o da un altro luogo in cui lo hai memorizzato
       const token = localStorage.getItem('token');
-      console.log('TOKEN', token)
+      console.log('Logged???', isLogged);
+      
 
       // Clona la richiesta e aggiungi l'header di autorizzazione con il token
       const authorizedRequest = request.clone({
@@ -25,12 +27,11 @@ export class AuthInterceptorService implements HttpInterceptor {
       });
 
       return next.handle(authorizedRequest);
+    } else {
+      // Se l'utente non è loggato, puoi gestire questa situazione come preferisci.
+      // Ad esempio, puoi reindirizzare l'utente alla pagina di login o generare un errore.
+      // In questo esempio, sto passando la richiesta direttamente al gestore HTTP senza alcuna modifica.
+      return next.handle(request);
     }
-
-    // Se la richiesta non è di tipo POST, PATCH o DELETE, passala direttamente al gestore HTTP
-    return next.handle(request);
   }
 }
-
-
-
